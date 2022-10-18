2b076b71897407abf73473b8ba013a4b9f08895b
BrieAnnaAllen <BrieAnnaNAllen@gmail.com>
2022-10-17 21:18:28 -0500

    Maid Type Addition
    Added maid type format in html and css.
     Started working on Javascript

---
+++ MaidRPG.js
@@ -0,0 +1,25 @@
+
+const athletics = document.getElementById('athletics')
+const athleticsValue = athletics.getAttribute('value')
+//change attribute value based on choice.
+function mtchoice(){
+    d = document.getElementById("maid-types-1").value;
+
+    switch(d){
+        case select:
+            break;
+        case lolita:
+            break;
+        case sexy:
+            break;
+        case pure:
+            break;
+        case cool:
+            break;
+        case boyish:
+            break;
+        case heroine:
+            break;
+        
+    }
+}
