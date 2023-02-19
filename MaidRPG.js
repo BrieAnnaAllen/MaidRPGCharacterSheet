295deab2b4b652b1ed60b2775edbf294d1a431cc
BrieAnnaAllen <BrieAnnaNAllen@gmail.com>
2023-02-19 14:17:44 -0600

    Roll
    Adding roll mechanic and display

--- MaidRPG.js
+++ MaidRPG.js
@@ -1,4 +1,9 @@
-function rollTwoDSix() {
-    console.log(Math.floor(Math.random() * 7));
-  // return Math.floor(Math.random() * 7);
+function rollTwoDSix(type) {
+  var first = (Math.floor(Math.random() * 6) +1);
+  var second = (Math.floor(Math.random() * 6) +1);
+  document.getElementById('display-result').textContent = "Results: " + 
+    first + " + " + 
+    second + " + " + 
+    document.getElementById(type).value + " = "+ 
+    (first + second + parseInt(document.getElementById(type).value));
 }
