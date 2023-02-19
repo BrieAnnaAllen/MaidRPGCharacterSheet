5299d336ba01a92a9bbb300eb8044e00273aa99a
BrieAnnaAllen <BrieAnnaNAllen@gmail.com>
2023-02-19 13:18:10 -0600

    Edits
    Added formatting, fixed font

--- MaidRPG.js
+++ MaidRPG.js
@@ -1,28 +1,4 @@
-
-const athletics = document.getElementById('athletics')
-const athleticsValue = athletics.getAttribute('value')
-//change attribute value based on choice.
-function mychoice(){
-    var mt1Dropdown = document.getElementById("maid-types-1");
-    currentOption = mt1Dropdown.options[mt1Dropdown.selectedIndex].value;
-
-    switch(d){
-        case select:
-            break;
-        case lolita:
-            athleticsValue -=1;
-            break;
-        case sexy:
-            break;
-        case pure:
-            break;
-        case cool:
-            break;
-        case boyish:
-            athleticsValue +=1;
-            break;
-        case heroine:
-            break;
-    }
-    athletics.setAttribute(athleticsValue);
+function rollTwoDSix() {
+    console.log(Math.floor(Math.random() * 7));
+  // return Math.floor(Math.random() * 7);
 }
